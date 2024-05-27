e375c721e8242ab4d204b39aadaa84c4e3f98162
Nika <white4ik1995@gmail.com>
2024-05-27 23:07:19 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BalloonComponent } from './components/balloon/balloon.component';
+import { IBalloon } from './balloon.interface';
+import { Balloon } from './balloon.class';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BalloonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ng-balloon-pop';
+  balloonsOnScreen = 3;
+  balloons: IBalloon[] = new Array(this.balloonsOnScreen)
+    .fill(0)
+    .map(() => new Balloon());
+    score = 0;
+    missed = 0
 }
---
+++ src/app/components/balloon/balloon.component.ts
@@ -0,0 +1,50 @@
+import { Component, ElementRef, OnInit, inject, input } from '@angular/core';
+import { IBalloon } from '../../balloon.interface';
+import { AnimationBuilder, animate, style } from '@angular/animations';
+
+@Component({
+  selector: 'app-balloon',
+  standalone: true,
+  imports: [],
+  templateUrl: './balloon.component.html',
+  styleUrl: './balloon.component.scss',
+})
+export class BalloonComponent implements OnInit {
+  balloon = input.required<IBalloon>();
+  animBuilder = inject(AnimationBuilder);
+  elRef = inject(ElementRef);
+
+  ngOnInit(): void {
+    this.animateBalloon();
+  }
+  animateBalloon() {
+    const buffer = 20;
+    const maxWidth =
+      window.innerWidth -
+      this.elRef.nativeElement.firstChild.clientWidth -
+      buffer;
+    const leftPosition = Math.floor(Math.random() * maxWidth);
+    const minSpeed = 5;
+    const speedVariation = 5
+    const speed = minSpeed + Math.random() * speedVariation;
+    const flyAnimation = this.animBuilder.build([
+      style({
+        translate: `${leftPosition}px 0`,
+        position: 'fixed',
+        left: 0,
+        bottom: 0,
+      }),
+      animate(
+        `${speed}s ease-out`,
+        style({
+          translate: `${leftPosition}px -100vh`,
+        })
+      ),
+    ]);
+    const player = flyAnimation.create(this.elRef.nativeElement.firstChild);
+    player.play();
+    player.onDone(() => {
+      console.log('animation done');
+    });
+  }
+}
